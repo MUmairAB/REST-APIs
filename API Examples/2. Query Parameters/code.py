c01c438dd15a93018ee84b8e74c8ff1728160c68
Umair Akram <120496694+MUmairAB@users.noreply.github.com>
2023-07-28 16:51:36 +0500

    Update and rename example2.py to code.py

--- API Examples/2. Query Parameters/code.py
+++ API Examples/2. Query Parameters/code.py
@@ -1,7 +1,6 @@
 """
-In this example, the parameters wont be the part of the path.
-Rather, the parameters will be passed as a querry string appended
-to the path.
+In this example, the function's parameters won't be passed as path parameters (won't be part of the path).
+Rather, the parameters will be passed as query parameters (a query string appended to the path).
 """
 import uvicorn
 from fastapi import FastAPI
@@ -15,7 +14,8 @@ async def base():
    return {"message": "Welcome to my web application"}
 
 #Define a function whose parameters will be passed
-# as a querry string
+# as a query parameters
+#  This function will return the student's CGPA given the name and age
 @app.get("/greet")
 async def hello(name:str, age:int):
    gpa = {"tom":3.0, "jerry":2.5, "spike":3.5}
@@ -25,7 +25,7 @@ async def hello(name:str, age:int):
       return {"name":name, "age":age, "CGPA":"Not in the Database"}
 
 if __name__ == "__main__":
-   uvicorn.run(app="example2:app",
+   uvicorn.run(app="code:app",
                host="127.0.0.1",
                port=8000,
                reload=True)
